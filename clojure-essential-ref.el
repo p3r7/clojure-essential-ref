546c1176c3b0ebd2ba6ecbacf73a2671691671c1
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-06-04 12:06:04 +0200

    Added CIDER as apackage dep

--- clojure-essential-ref.el
+++ clojure-essential-ref.el
@@ -4,7 +4,7 @@
 ;;
 ;; Version: 0.1.0
 ;; URL: https://github.com/p3r7/clojure-essential-ref
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "24")(cider "0.24.0"))
 ;;
 ;; SPDX-License-Identifier: MIT
 
